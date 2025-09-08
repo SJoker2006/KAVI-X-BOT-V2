1619cebe8cf3c3d43c0d225f1a6f58ae5ddd5f80
SJoker2006 <lahiruimesh847@gmail.com>
2025-09-08 22:38:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~7hAghKjS#hZhy4619YrLZqCZ-Wi-dUa1b97ckOW6CTamIPRVJ7QE"
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~T8Rj1aRA#apmcqvL_Av6ZYd3aPAJuE4PG7hAci9Auwir2PeMZnd8"
 global.mongodburi = process.env.MONGODB_URI || "mongodb+srv://ketkshqAqjXygEF5:ketkshqAqjXygEF5@freeuri.dmkyoml.mongodb.net/helloworld123456789?retryWrites=true&w=majority&appName=freeuri"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94728380062"
